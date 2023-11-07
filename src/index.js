6a9a3fafce705364e9f70ac9692a2460477ba9bd
Nick Sette <njs2394@gmail.com>
2023-11-07 15:40:35 -0500

    3.5

--- src/index.js
+++ src/index.js
@@ -5,34 +5,36 @@ const url = "http://localhost:3000/movies"
 let movies = ""
 
 fetch(url)
-.then(Response => Response.json())
-.then(data => {
-    console.log(data)
-    for (imgLink of data) {
-        console.log(imgLink.image)
-        renderImg(imgLink.image)
-    }
-    
-    renderSelectedMovie(data[0])
+    .then(Response => Response.json())
+    .then(data => {
+        // console.log(data)
+        movies = data // This a key cog is using it on line 24
+        for (imgLink of data) {
+            // console.log(imgLink.image)
+            renderImg(imgLink.image)
+        }
 
-    // Title, Yr. desc
+        renderSelectedMovie(data[0])
 
-    
-})
+
+    })
 
 movieListContainer.addEventListener("click", (e) => {
     // debugger
-    console.log(e.target)
+    console.log(movies)
+    console.log(e.target.src) // Image value
+    const results = movies.find(({ image }) => e.target.src.includes(image.substring(1)))
+    renderSelectedMovie(results)
 })
 
 
-function renderImg (el) {
+function renderImg(el) {
     const img = document.createElement("img")
     img.src = el
     movieListContainer.appendChild(img)
 }
 
-function renderSelectedMovie (el) {
+function renderSelectedMovie(el) {
     const movieImgContainer = document.getElementById("detail-image")
     const movieTitleContainer = document.getElementById("title")
     const movieYearContainer = document.getElementById("year-released")
