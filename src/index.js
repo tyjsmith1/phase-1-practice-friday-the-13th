a132fb5bcae04ec9d01d53a9ac7b5994d52d5ce8
tylersmith <tyjsmith1@gmail.com>
2023-11-07 14:06:06 -0600

    tylers code

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-//sup
+// Tyler's code
+
+const movieListContainer = document.getElementById("movie-list")
+const url = "http://localhost:3000/movies"
+let movies = ""
+
+fetch(url)
+.then(Response => Response.json())
+.then(data => {
+    console.log(data)
+    for (imgLink of data) {
+        console.log(imgLink.image)
+        renderImg(imgLink.image)
+    }
+    
+    renderSelectedMovie(data[0])
+
+    // Title, Yr. desc
+
+    
+})
+
+movieListContainer.addEventListener("click", (e) => {
+    // debugger
+    console.log(e.target)
+})
+
+
+function renderImg (el) {
+    const img = document.createElement("img")
+    img.src = el
+    movieListContainer.appendChild(img)
+}
+
+function renderSelectedMovie (el) {
+    const movieImgContainer = document.getElementById("detail-image")
+    const movieTitleContainer = document.getElementById("title")
+    const movieYearContainer = document.getElementById("year-released")
+    const movieDescContainer = document.getElementById("description")
+    movieImgContainer.src = el.image
+    movieTitleContainer.textContent = el.title
+    movieYearContainer.textContent = el.release_year
+    movieDescContainer.textContent = el.description
+
+}
+
